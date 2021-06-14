806e2da253ab1b56d85438dde6c0ba2ecb361cc0
Jonathan Sutton <jonathans@al.insper.edu.br>
2021-06-13 21:34:12 -0300

    arquivos de teste

---
+++ test/br/edu/insper/desagil/pf/classeTest.java
@@ -0,0 +1,14 @@
+package br.edu.insper.desagil.pf;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class classeTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
